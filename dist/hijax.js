45bf36c4728facd552b6cc9a1046ce5fd34dae87
Donniel Thomas <donniel@mobify.com>
2014-06-18 14:40:39 -0700

    Updated distribution files with latest changes

--- dist/hijax.js
+++ dist/hijax.js
@@ -12,8 +12,11 @@
         root.Hijacker = factory();
     }
 }(this, function() {
-    function Hijacker(name, condition, callbacks) {
-        if (!name || !condition || !callbacks || typeof callbacks !== 'object') {
+    function Hijacker(name, condition, callbacks, options) {
+        options = options || {};
+
+        if (!name || !condition || !callbacks ||
+            typeof callbacks !== 'object') {
             throw 'Missing or invalid Hijacker proxy initialization options';
         }
 
@@ -27,6 +30,24 @@
             complete: []
         };
 
+        var defaultParsers = {
+            '* text': String,
+            'text html': function(html) { return html; },
+            'text json': JSON.parse
+        };
+
+        // Custom parsers for data types
+        var parsers = this.dataParsers = defaultParsers;
+        var customParsers = options.dataParsers;
+
+        if (customParsers) {
+            for(var parser in customParsers) {
+                if(customParsers.hasOwnProperty(parser)) {
+                    this.dataParsers[parser] = customParsers[parser];
+                }
+            }
+        }
+
         // Set the callbacks that have been provided
         for (var event in callbacks) {
             if (callbacks.hasOwnProperty(event)) {
@@ -37,15 +58,132 @@
         return this;
     }
 
+    var states = {
+        UNSENT: 0,
+        OPENED: 1,
+        HEADERS_RECEIVED: 2,
+        LOADING: 3,
+        DONE: 4
+    };
+
+    // Get a particular response header value by key
+    Hijacker.prototype.getResponseHeader = function(xhr, key) {
+        var rHeaders = /^(.*?):[ \t]*([^\r\n]*)$/mg;
+        var match;
+        var responseHeadersString;
+
+        if ([states.HEADERS_RECEIVED, states.LOADING, states.DONE]
+                .indexOf(xhr.readyState) > -1) {
+
+            responseHeadersString = xhr.getAllResponseHeaders();
+
+            if (!this.responseHeaders) {
+                this.responseHeaders = {};
+                while ((match = rHeaders.exec(responseHeadersString))) {
+                    this.responseHeaders[match[1].toLowerCase()] = match[2];
+                }
+            }
+            match = this.responseHeaders[key.toLowerCase()];
+        }
+        return match === null ? null : match;
+    };
+
+    // Data parse methods adapted from jQuery 2.1.1
+    function getResponses(xhr) {
+        var responses = {};
+        var responseFields = {
+            'text': 'responseText',
+            'json': 'responseJSON'
+        };
+
+        // What responses are available?
+        for (var rType in responseFields) {
+            if (xhr.hasOwnProperty(responseFields[rType])) {
+                responses[rType] = xhr[responseFields[rType]];
+            }
+        }
+
+        return responses;
+    }
+
+    // Check which response types have been provided by the server
+    Hijacker.prototype.detectResponseTypes = function(xhr) {
+        var detectedTypes = [];
+        var contentType = this.getResponseHeader(xhr, 'Content-Type');
+        var knownTypes = {
+            'html': /html/,
+            'json': /json/,
+            'script': /(?:java|ecma)script/
+        };
+
+        // Check if this is a known data type, and add it to the stack
+        for (var type in knownTypes) {
+            if (knownTypes[type] && knownTypes[type].test(contentType)) {
+                detectedTypes.unshift(type);
+                break;
+            }
+        }
+
+        return detectedTypes;
+    };
+
+    // Detect the type of response, and convert it to a usable type
+    Hijacker.prototype.parseResponse = function(xhr) {
+        var parsers = this.dataParsers;
+        var rHeaders = this.detectResponseTypes(xhr);
+        var responses = getResponses(xhr);
+
+        var firstDataType, finalDataType;
+        var rHeaderType = rHeaders[0];
+
+        // Processed response exists, for eg., responseJSON
+        if (rHeaderType in responses) {
+            finalDataType = rHeaderType;
+        } else {
+            // Can we convert the response?
+            // TODO: Refactor to fix lack of hasOwnProperty check
+            /*jshint forin: false */
+            for (var type in responses) {
+                // Unable to find a response header type
+                if (!rHeaderType) {
+                    finalDataType = type;
+                    break;
+                } else if (parsers[type + ' ' + rHeaderType]) {
+                    // We found a converter for the specified type! We should
+                    // use it
+                    return parsers[type + ' ' + rHeaderType](responses[type]);
+                }
+                if (!firstDataType) {
+                    firstDataType = type;
+                }
+            }
+            // Or just use first one
+            finalDataType = finalDataType || firstDataType;
+        }
+
+        if (finalDataType) {
+            if (finalDataType !== rHeaderType) {
+                rHeaderType.unshift(finalDataType);
+            }
+            return responses[finalDataType];
+        }
+
+        return xhr.response;
+    };
+
     // Handles an XHR event, like beforeSend, receive or complete
     Hijacker.prototype.fireEvent = function(event, xhr) {
         var eventCallbacks = this.callbacks[event];
-        var data = xhr.response;
 
         if (!this.condition(xhr.url)) { return; }
 
         for (var ctr = 0; ctr < eventCallbacks.length; ctr++) {
-            eventCallbacks[ctr].call(this, data, xhr.statusText, xhr);
+            if (event === 'complete') {
+                // Include parsed data
+                eventCallbacks[ctr].call(this, this.parseResponse(xhr), xhr);
+            } else {
+                eventCallbacks[ctr].call(this, xhr);
+            }
         }
     };
 
@@ -57,7 +195,6 @@
     return Hijacker;
 }));
 
-/* global Hijacker */
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
@@ -77,30 +214,32 @@
     /**
      * Proxy destFn, so that beforeFn runs before it, and afterFn runs after it
      *
-     * @destFn {Function}: Target
-     * @beforeFn {Function}: (optional) Runs before destFn
-     * @afterFn {Function}: (optional) Runs after destFn
+     * @destFn {Function}:      Target
+     * @beforeFn {Function}:    (optional) Runs before destFn
+     * @afterFn {Function}:     (optional) Runs after destFn
      */
     function proxyFunction(destFn, beforeFn, afterFn) {
-        var _destFn = destFn;
-        return function() {
+        var proxied = function() {
             var result;
 
             if (typeof destFn !== 'function') {
                 throw destFn + ' is not a function, and cannot be proxied!';
             }
-
             if (typeof beforeFn === 'function') {
                 beforeFn.apply(this, arguments);
             }
-
-            result = _destFn.apply(this, arguments);
+            result = destFn.apply(this, arguments);
             if (typeof afterFn === 'function') {
                 afterFn.apply(this, arguments);
             }
 
             return result;
         };
+
+        // Allows accessing original function through our proxy
+        proxied._original = destFn;
+
+        return proxied;
     }
 
     // XHR states
@@ -207,7 +346,7 @@
                     xhr.onload, receiveHandler, completeHandler
                 );
                 xhr.proxied = true;
-            } else if (xhr.readyState === states.LOADING) {
+            } else if (xhr.readyState === states.HEADERS_RECEIVED) {
                 receiveHandler();
             } else if (xhr.readyState === states.DONE) {
                 completeHandler();
@@ -223,4 +362,5 @@
 
     return hijax;
 }));
+
 
